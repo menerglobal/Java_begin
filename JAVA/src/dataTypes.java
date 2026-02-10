fc61deff9918d5ef21941128f8d60548bda8ea11
Mehmet Enes Erdogan <77643160+menerglobal@users.noreply.github.com>
2026-02-10 22:22:13 +0100

    Add files via upload

---
+++ JAVA/src/dataTypes.java
@@ -0,0 +1,12 @@
+public class dataTypes {
+    public static void main(String[] args) {
+        int number = 12;
+        number = -120;
+        double number1 = 12.5;
+        number1 = 278;
+        char character = 'A';
+
+        boolean isTrue = false;
+        if (isTrue) == true;
+    }
+}
---
+++ JAVA/src/javaDemos.java
@@ -0,0 +1,10 @@
+public class javaDemos {
+    public static void main(String[] args) {
+        int studentNumber = 12;
+        String message ="Student number: ";
+        System.out.println("Hello Java");
+        System.out.println(message +studentNumber);
+
+
+    }
+}
